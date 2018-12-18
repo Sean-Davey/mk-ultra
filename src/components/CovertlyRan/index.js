5b97f251a23b3a5d962fd95b3120982782524579
Sean Davey <seananthonydavey@gmail.com>
2018-12-18 02:49:40 +0000

    Covert component finished and secretoperation component

---
+++ src/components/CovertlyRan/index.js
@@ -0,0 +1,14 @@
+import React , { Component } from 'react';
+
+class CovertlyRan extends Component {
+
+    render() {
+        return(
+        <div className="c-mkultra">
+    <p> In excess of 150 experiments were carried out in over 80 different institutions including universities, prisons, hospitals and pharmaceutical companies, some of which without the knowledge of the <span onClick={null} className="redacted">CIA's</span> presence and <span onClick={null} className="redacted"> grotesque human experimentation </span> they had become the host to.</p>
+        </div>            
+            )
+        }
+    };
+    
+    export default CovertlyRan;
---
+++ src/components/InstitutionAwareness/index.js
@@ -0,0 +1,14 @@
+import React , { Component } from 'react';
+
+class InstitutionAwareness extends Component {
+
+    render() {
+        return(
+            <div className="c-mkultra">
+            <p> Those institutions who were aware of the <span onClick={null} class="redacted">CIA's operations </span> acted in accordance, believing that they were 'aiding their government' according to a <a href="https://www.andrew.cmu.edu/user/rp3h/lansberry/mkultra.pdf">Senate report</a> revealed in later years. </p>
+            </div>
+        )
+    }
+};
+
+export default InstitutionAwareness;
